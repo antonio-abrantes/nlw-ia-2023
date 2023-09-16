d45f34f92149c650f32a3644b13bba119a73344d
antonio-abrantes <antonio-abrantes@hotmail.com.br>
2023-09-15 22:14:37 -0300

    finished api

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,15 +1,23 @@
 import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors'
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
 
-const app = fastify();
+const app = fastify()
 
-app.get("/", () => {
-  return "Hello world";
-});
+app.register(fastifyCors, {
+  origin: '*'
+})
 
-app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log("HTTP Server Is Running...");
-  });
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAiCompletionRoute)
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('HTTP Server Running!')
+})
